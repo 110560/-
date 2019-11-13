58f155b35a5d8e9c5229d51b0524d86a7682b9a8
110560 <32481672+110560@users.noreply.github.com>
2019-11-13 15:55:59 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 安装软件
+apt-get update
+apt-get install -y git supervisor vnstat
+
+# 配置dns
+echo -e "options timeout:1 attempts:1 rotate\nnameserver 8.8.8.8\nnameserver 8.8.4.4" >/etc/resolv.conf
+
+# 切换时区
+timedatectl set-timezone Asia/Shanghai
+
+
+cd ~
+git clone https://github.com/LEE-Blog/shadowsocksr.git
+cd shadowsocksr
+sh setup_cymysql2.sh
+sh initcfg.sh
+
+
+cat  >> /etc/supervisor/conf.d/ssr.conf <<EOF
+[program:ssr]
+command=python /root/shadowsocksr/server.py 
+autorestart=true
+autostart=true
+user=root
+EOF
+
+
+echo "ulimit -n 102400" >> /etc/default/supervisor
+/etc/init.d/supervisor restart
+supervisorctl restart ssr
+
+vnstat -u -i eth0
+
+systemctl vnstat start
+
+chown -R vnstat:vnstat /var/lib/vnstat
+
+/etc/init.d/vnstat restart
