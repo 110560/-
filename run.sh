e066e23f41f88e314bdee6df8e2d62a17e1bfab6
duruo <32481672+110560@users.noreply.github.com>
2020-03-24 14:18:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,7 @@
 apt-get update
 apt-get install -y git supervisor vnstat
 apt-get install net-tools -y
-wget https://cdn.ipip.net/17mon/besttrace4linux.zip
 
-# 路由跟踪
-unzip besttrace4linux.zip
-chmod +x besttrace
 
 # 配置dns
 echo -e "options timeout:1 attempts:1 rotate\nnameserver 8.8.8.8\nnameserver 8.8.4.4" >/etc/resolv.conf
