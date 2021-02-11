d493bf0942b2b5cbcb707cf21be573d2501def48
duruo <32481672+110560@users.noreply.github.com>
2021-02-12 00:38:54 +0800

    Create run2.sh

---
+++ run2.sh
@@ -0,0 +1,24 @@
+apt-get install -y git supervisor vnstat
+apt-get install net-tools -y
+echo -e "options timeout:1 attempts:1 rotate\nnameserver 8.8.8.8\nnameserver 8.8.4.4" >/etc/resolv.conf
+timedatectl set-timezone Asia/Shanghai
+
+cat  >> /etc/supervisor/conf.d/ssr.conf <<EOF
+[program:ssr]
+command=python /root/shadowsocksr/server.py 
+autorestart=true
+autostart=true
+user=root
+EOF
+
+echo "ulimit -n 102400" >> /etc/default/supervisor
+/etc/init.d/supervisor restart
+supervisorctl restart ssr
+
+vnstat -u -i eth0
+
+systemctl vnstat start
+
+chown -R vnstat:vnstat /var/lib/vnstat
+
+/etc/init.d/vnstat restart
