c1f7c15a48bbbec47e5f0f2f835457f3cc0c6746
du <32481672+110560@users.noreply.github.com>
2024-03-15 20:35:23 +0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,6 +1,6 @@
 apt-get install -y supervisor vnstat
 apt-get install net-tools -y
-echo -e "options timeout:1 attempts:1 rotate\nnameserver 1.1.1.1 \nnameserver 1.0.0.1 " >/etc/resolv.conf;
+echo -e "options timeout:1 attempts:1 rotate\nnameserver 1.1.1.1 \nnameserver 208.67.222.222 " >/etc/resolv.conf;
 timedatectl set-timezone Asia/Shanghai
 #echo "0 6 * * * supervisorctl restart ssr" >> /var/spool/cron/crontabs/root
 
