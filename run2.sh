ac9fcc4f33e2862ec126f3bd87aff3506d8c92e7
du <32481672+110560@users.noreply.github.com>
2024-09-19 03:02:44 +0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,12 +1,11 @@
-apt-get install -y supervisor vnstat
-apt-get install net-tools -y
-echo -e "options timeout:1 attempts:1 rotate\nnameserver 1.1.1.1 \nnameserver 208.67.222.222 " >/etc/resolv.conf;
-timedatectl set-timezone Asia/Shanghai
-#echo "0 6 * * * supervisorctl restart ssr" >> /var/spool/cron/crontabs/root
+apt-get update && apt-get install -y supervisor vnstat net-tools
 
-cat  >> /etc/supervisor/conf.d/ssr.conf <<EOF
+echo -e "options timeout:1 attempts:1 rotate\nnameserver 1.1.1.1\nnameserver 208.67.222.222" >/etc/resolv.conf
+timedatectl set-timezone Asia/Shanghai
+
+cat >> /etc/supervisor/conf.d/ssr.conf <<EOF
 [program:ssr]
-command=python /root/shadowsocksr/server.py 
+command=python /root/shadowsocksr/server.py
 autorestart=true
 autostart=true
 user=root
@@ -16,10 +15,4 @@ echo "ulimit -n 102400" >> /etc/default/supervisor
 /etc/init.d/supervisor restart
 supervisorctl restart ssr
 
-vnstat -u -i eth0
-
-systemctl vnstat start
-
-chown -R vnstat:vnstat /var/lib/vnstat
-
-/etc/init.d/vnstat restart
+(crontab -l 2>/dev/null | grep -qFx "0 6 * * * supervisorctl restart ssr") || (echo "0 6 * * * supervisorctl restart ssr" | crontab -)
