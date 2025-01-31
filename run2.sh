9d4213645e4307f5292f986d4a4045632374e996
du <32481672+110560@users.noreply.github.com>
2025-01-31 18:26:17 +0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,7 +1,6 @@
-apt-get update && apt-get install -y supervisor vnstat net-tools
+# Py2.7下 dns 修改 守护进程
 
 echo -e "options timeout:1 attempts:1 rotate\nnameserver 1.1.1.1\nnameserver 208.67.222.222" >/etc/resolv.conf
-timedatectl set-timezone Asia/Shanghai
 
 cat >> /etc/supervisor/conf.d/ssr.conf <<EOF
 [program:ssr]
