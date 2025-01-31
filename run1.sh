a69d1dde94d5b5f933623a671118ab2504b8cafc
du <32481672+110560@users.noreply.github.com>
2025-01-31 20:00:14 +0800

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,6 +1,6 @@
 # py2.7环境下克隆 ShadowsocksR 仓库的 1 分支并且安装
 cd ~
-git clone -b 1 https://github.com/110560/shadowsocksr.git
+git clone https://github.com/110560/ShadowsocksR-py2.7.git shadowsocksr
 cd shadowsocksr
 apt-get install python-pip -y
 pip install -r requestment.txt
